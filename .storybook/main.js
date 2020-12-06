c8bdf0fdbd30a4037b6932c0dbd5114316e5842b
Rildo Candido <rildocandido@yahoo.com>
2020-12-06 12:26:20 -0200

    Configurando o dependabot

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,12 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)",
-    "../src/components/**/stories.tsx"
-  ],
-  "addons": [
-    "@storybook/addon-essentials",
-  ]
+  stories: ['../src/components/**/stories.tsx'],
+  addons: ['@storybook/addon-essentials'],
+  babel: async (options) => ({
+    ...options,
+    plugins: [...options.plugins, '@babel/plugin-transform-react-jsx']
+  }),
+  webpackFinal: (config) => {
+    config.resolve.modules.push(`${process.cwd()}/src`)
+    return config
+  }
 }
